552b410b09149b68650694b0185d348b47abd414
diogo <diogo.pinto@itgest.pt>
2024-02-21 11:38:52 +0000

    first

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TodoView from '../views/TodoView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/vue-todos',
+      name: 'home',
+      component: TodoView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/testing',
+      name: 'testing',
+      component: () => import('../views/TestingView.vue'),
+    }
+  ]
+})
+
+export default router
