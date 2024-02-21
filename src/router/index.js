d876a57b8843acd572df98db3878feb5c6abd9c2
diogo <diogo.pinto@itgest.pt>
2024-02-21 11:47:45 +0000

    second

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/vue-todos',
+      path: '/vue3-todos-c-c',
       name: 'home',
       component: TodoView,
     },
@@ -15,12 +15,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/AboutView'),
     },
     {
       path: '/testing',
       name: 'testing',
-      component: () => import('../views/TestingView.vue'),
+      component: () => import('../views/TestingView'),
     }
   ]
 })
